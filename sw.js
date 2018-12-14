9178495a7c412c39e6573f756fc989832f8faebc
Vaseem ishak <vaseem@mnm.com.tr>
2018-12-14 13:30:32 +0300

    cache added

--- sw.js
+++ sw.js
@@ -1,3 +1,19 @@
+var CACHE_NAME = 'my-site-cache-v1';
+var urlsToCache = [];
+
 self.addEventListener('fetch', function(event) {
     console.log(event.request.url);
+    urlsToCache.push(event.request.url);
+});
+
+
+self.addEventListener('install', function(event) {
+    // Perform install steps
+    event.waitUntil(
+        caches.open(CACHE_NAME)
+            .then(function(cache) {
+                console.log('Opened cache');
+                return cache.addAll(urlsToCache);
+            })
+    );
 });
